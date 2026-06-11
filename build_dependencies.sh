8b5103a912974b75dfef3dd5932771e57fa3d8bc
mravi105 <madhusuthanan_ravi@comcast.com>
2026-06-11 20:08:00 +0000

    build configuration changes for L1 test and native build

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -21,7 +21,7 @@ cd ..
 
 git clone --branch R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
-git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
+git clone --branch feature/RDKEMW-6078_DeviceSettings_Interface https://github.com/rdkcentral/entservices-apis.git
 git clone --branch 1.0.14 https://github.com/rdkcentral/entservices-testframework.git
 git clone --branch main https://github.com/rdkcentral/rdk-halif-device_settings.git
 
