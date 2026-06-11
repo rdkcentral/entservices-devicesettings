ad864b12a0dfd091df566f0597b3f463f5448ca4
mravi105 <madhusuthanan_ravi@comcast.com>
2026-06-11 06:55:06 +0000

    Build Dependencies issues solved for entservices-devicesettings plugin

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -13,6 +13,7 @@ pip install jsonref
 git clone --branch R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
+git clone --branch 1.0.14 https://github.com/rdkcentral/entservices-testframework.git
 
 echo "======================================================================================"
 echo "building thunderTools"
